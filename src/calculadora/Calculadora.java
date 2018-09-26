f27c61a5e82a6efe5f38cad6a3187e61b9506122
Lorente35 <Lorente35@LAPTOP-30B2IGTD>
2018-09-26 16:34:15 +0200

    Suma

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -10,11 +10,21 @@ public class Calculadora {
 
         int num1, num2;
 
-        System.out.print("Fica un numero: ");
+        System.out.println("Numero1: ");
         num1 = in.nextInt();
-        System.out.print("Fica un numero: ");
-        num2 = in.nextInt();
 
+        System.out.println("Numero2: ");
+        num2 = in.nextInt();
+        
+        System.out.println ("Suma: " + num1 + num2 + "= " + suma);
+    
+
+    public static int suma(int num1, int num2) {
+
+        int suma = num1 + num2;
+
+        return suma;
     }
 
+}
 }
