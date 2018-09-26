0f1fcb7bfe00dbc1f10774fc04be8b0b823472bc
Lorente35 <Lorente35@LAPTOP-30B2IGTD>
2018-09-26 16:19:58 +0200

    Pedir 2 numeros

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,8 +1,20 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int num1, num2;
+
+        System.out.print("Fica un numero: ");
+        num1 = in.nextInt();
+        System.out.print("Fica un numero: ");
+        num2 = in.nextInt();
+
     }
 
 }
